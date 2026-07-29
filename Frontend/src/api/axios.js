5c3a157f56b5f52308fbaebe0981ace4a1bef152
Tech <Tech@UI-TC-16418-P.utopia.local>
2026-07-29 15:50:25 +0500

    Updating API and config issue

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://98.94.6.13:8080/api', // Live AWS backend URL
+    baseURL: '/api', // Nginx automatically ise port 8080 par bhej dega bina CORS error ke
 });
 
 // Interceptor: JWT token ko automatically har request mein daalne ke liye
