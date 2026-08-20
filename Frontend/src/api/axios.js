c94c4aa8189043ba2ca1ad11db4945d9b8a09e3d
Tech <Tech@UI-TC-16418-P.utopia.local>
2026-08-20 10:35:42 +0500

    db update db/

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api',
+    baseURL: 'http://incubatorsystem2-779054019.us-east-1.elb.amazonaws.com/api',
     headers: {
         'Content-Type': 'application/json',
     }
